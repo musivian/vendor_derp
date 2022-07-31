00ded1c64827b97a35609db82e1ae5bc8a8a66a9
El Dainosor <eldainosor@gmail.com>
2022-07-31 05:38:19 -0300

    config: Disable DocumentsUI icon
    Change-Id: I658958aca6014d62bc68e231f92e857d5b899af5

--- config/common.mk
+++ config/common.mk
@@ -54,6 +54,10 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/derp/prebuilt/common/etc/init/init.derp-system_ext.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.derp-system_ext.rc
 
+# our sysconfig
+PRODUCT_COPY_FILES += \
+    vendor/derp/config/permissions/derpfest-sysconfig.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/derpfest-sysconfig.xml
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.sip.voip.xml

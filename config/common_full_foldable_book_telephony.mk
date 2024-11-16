The-Clover-Project/vendor_clover

overlay: Disable advanced rotation settings for foldable_book
Change-Id: I947e00328ce117e628fb7622c644b38d1478ab2f

config/common_full_foldable_book_telephony.mk
@@ -9,3 +9,5 @@ PRODUCT_PRODUCT_PROPERTIES += \
 $(call inherit-product, vendor/lineage/config/tablet.mk)
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/foldable_book

musivian/vendor_derp

config/common_full_foldable_book_telephony.mk
@@ -9,3 +9,5 @@ PRODUCT_PRODUCT_PROPERTIES += \
 $(call inherit-product, vendor/derp/config/tablet.mk)
 
 $(call inherit-product, vendor/derp/config/telephony.mk)
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/derp/overlay/foldable_book
0548cff109f27455ff33676dc6197d2b4232107d
NurKeinNeid <mralexman3000@gmail.com>
2024-11-16 21:56:35 +0100

    config: Move the textclassifier models to common_mobile
    Change-Id: Id29dabe18fb8f76efdffc45e74d61bbe67470882
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -44,3 +44,10 @@ PRODUCT_SYSTEM_PROPERTIES += \
 # SystemUI plugins
 PRODUCT_PACKAGES += \
     QuickAccessWallet
+
+# TextClassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_annotator_en_model \
+    libtextclassifier_annotator_universal_model \
+    libtextclassifier_actions_suggestions_universal_model \
+    libtextclassifier_lang_id_model
